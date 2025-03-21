b37dced95c3e914ab729a8f9e4a5654c95bb969f
Sparsh Maheshwari <sparshmaheshwari15@gmail.com>
2025-03-21 21:35:31 +0530

    Comment out console log for database URL in dbConnection.js

--- dbConnection.js
+++ dbConnection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const dbUrl = process.env.LOCALDB_URL;
 
-console.log(dbUrl)
+// console.log(dbUrl)
 main()
     .then((res) => {
         console.log("Connection Successful to DB");
